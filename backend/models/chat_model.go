c1b0b5e95855fd7d0c0931264f5bf90ad2ffe064
Junyu Zhou <114630887+CHNdominic@users.noreply.github.com>
2024-04-21 21:30:53 +0800

    Add files via upload

---
+++ backend/models/chat_model.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/websocket"
+	"sync"
+)
+
+var (
+	Upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	MsgChan = make(chan Message) // 定义全局的消息通道
+
+	// 建立一个WebSocket连接的映射，并使用互斥锁进行保护
+	Conns = struct {
+		sync.RWMutex
+		M map[*websocket.Conn]string
+	}{M: make(map[*websocket.Conn]string)}
+)
+
+type Message struct {
+	Sender  string
+	Content string
+}
