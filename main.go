f59f5e0514a3a1455ac16ee2083650036ee76df3
NS <sc21sn@leeds.ac.uk>
2024-04-28 22:57:35 +0800

    [ADD] change the name of document root, and add a route group.

--- main.go
+++ main.go
@@ -10,41 +10,44 @@ func main() {
 	engine := gin.Default()
 
 	// 中间件
-	engine.Use(JwtToken())
-	engine.Use(Cors())
+	//engine.Use(JwtToken())
+	//engine.Use(Cors())
 	// 数据库
-	InitDB()
+	//InitDB()
 
-	engine.Static("/static", "frontend/static")
-	engine.LoadHTMLGlob("frontend/*.html")
+	engine.Static("/static", "frontend(web)/static")
+	engine.LoadHTMLGlob("frontend(web)/*.html")
 
-	engine.GET("/", ToIndex)
-	engine.GET("/index", ToIndex)
+	webGroup := engine.Group("/web")
+	{
+		webGroup.GET("/", ToIndex)
+		webGroup.GET("/index", ToIndex)
 
-	engine.GET("/login", ToLogin)
-	engine.GET("/register", ToRegister)
+		webGroup.GET("/login", ToLogin)
+		webGroup.GET("/register", ToRegister)
+		webGroup.GET("/profile/:uid", func(c *gin.Context) {
+			uid := c.Param("uid")
+			ToProfile(c, uid)
+		})
+
+		webGroup.GET("/vip", ToVip)
+		webGroup.GET("/bill", ToBill)
+
+		webGroup.GET("/download", ToDownload)
+
+		webGroup.GET("/help", ToHelp)
+		webGroup.GET("/help/:searchText", func(c *gin.Context) {
+			searchText := c.Param("searchText")
+			ToHelpDetails(c, searchText)
+		})
+
+		webGroup.GET("/messages", ToMessages)
+		webGroup.GET("/notifications", ToNotifications)
+		webGroup.GET("/setting", ToSetting)
+	}
+
 	engine.POST("/login", Login)
 	engine.POST("/register", Register)
-
-	engine.GET("/profile/:uid", func(c *gin.Context) {
-		uid := c.Param("uid")
-		ToProfile(c, uid)
-	})
-
-	engine.GET("/vip", ToVip)
-	engine.GET("/bill", ToBill)
-
-	engine.GET("/download", ToDownload)
-
-	engine.GET("/help", ToHelp)
-	engine.GET("/help/:searchText", func(c *gin.Context) {
-		searchText := c.Param("searchText")
-		ToHelpDetails(c, searchText)
-	})
-
-	engine.GET("/messages", ToMessages)
-	engine.GET("/notifications", ToNotifications)
-	engine.GET("/setting", ToSetting)
 
 	err := engine.Run()
 	if err != nil {
