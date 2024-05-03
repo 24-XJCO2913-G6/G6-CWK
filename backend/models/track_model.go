071bf8a0a333ec9064ef69d569a8178526527ce7
NS <sc21sn@leeds.ac.uk>
2024-05-04 05:57:51 +0800

    [ADD] Add the function of track model

--- backend/models/track_model.go
+++ backend/models/track_model.go
@@ -1,1 +1,34 @@
 package models
+
+type Track struct {
+	Tid         string  `xorm:"pk"` // 轨迹的唯一标识符，被标记为主键
+	Uid         string  // 用户的唯一标识符
+	StrTime     string  // 轨迹的起始时间
+	EndTime     string  // 轨迹的结束时间
+	Distance    float64 // 轨迹的距离
+	Coordinates string  // 轨迹的坐标信息
+	// 如[x,y],[x,y]
+}
+
+func BuildModelTrack() {
+	err := Db.Sync2(new(Track))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AddTrack(Uid string, StrTime string, EndTime string, Distance float64, Coordinates string) int64 {
+	track := &Track{
+		Uid:         Uid,
+		StrTime:     StrTime,
+		EndTime:     EndTime,
+		Distance:    Distance,
+		Coordinates: Coordinates,
+	}
+
+	Tid, err := Db.Insert(track)
+	if err != nil {
+		return -1
+	}
+	return Tid
+}
