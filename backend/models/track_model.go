db10766fd38225fe7f9aec805b2532a9a9952b10
NS <sc21sn@leeds.ac.uk>
2024-05-04 22:14:08 +0800

    [ADD] Date update

--- backend/models/track_model.go
+++ backend/models/track_model.go
@@ -1,5 +1,7 @@
 package models
 
+import "strings"
+
 type Track struct {
 	Tid         string  `xorm:"pk"` // 轨迹的唯一标识符，被标记为主键
 	Uid         string  // 用户的唯一标识符
@@ -31,4 +33,16 @@ func AddTrack(Uid string, StrTime string, EndTime string, Distance float64, Coor
 		return -1
 	}
 	return Tid
+}
+
+func CombineTime(date string, time string) string {
+	dateTime := date + " " + time
+
+	return dateTime
+}
+
+func DivideTime(dateTime string) (string, string) {
+	times := strings.Split(dateTime, " ")
+
+	return times[0], times[1]
 }
