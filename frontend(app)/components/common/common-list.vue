3279bba8f1703a95d8b816ee03812d935b745977
Vero060830 <126401087+Vero060830@users.noreply.github.com>
2024-05-04 18:45:02 +0800

    [FIX] Fix the bug of demonstration of map

--- frontend(app)/components/common/common-list.vue
+++ frontend(app)/components/common/common-list.vue
@@ -1,27 +1,36 @@
 <template>
-	<view class="p-2 animated fast fadeIn">
+	
+	<view class="p-2 animated fast fadeIn" @click='openDetail'>
+			
 		<!-- 头像昵称 | 关注按钮 -->
 		<view class="flex align-center justify-between">
 			<view class="flex align-center">
 				<!-- 头像 -->
 				<image class="rounded-circle mr-2" 
-				:src="item.userpic" @click="openSpace(item.user_id)"
+				:src="item.userpic" 
 				style="width: 65rpx;height: 65rpx;" 
 				lazy-load></image>
+				<!-- vip -->
 				<!-- 昵称发布时间 -->
 				<view>
 					<view class="font" style="line-height: 1.5;">
-						{{item.username}}
+						{{item.name}}
 					</view>
 					<text class="font-sm text-light-muted" 
 					style="line-height: 1.5;">
-						{{item.newstime|formatTime}}
+						{{item.time}}
 					</text>
 				</view>
+				<view v-if = "item.visibility== 1">
+					<image src="../../static/images/VIP.png" style="width: 25px; height: 25px; background-color: white; margin-bottom: 2px;">
+				</image>
+				</view>
+				
 			</view>
+	
 			<!-- 按钮 -->
-			<view @click="follow" 
-			v-if="!item.isFollow && user.id !== item.user_id"
+			<view
+			
 			class="flex align-center justify-center rounded bg-main text-white animated faster" 
 			style="width: 90rpx;height: 50rpx;"
 			hover-class="jello"
@@ -29,37 +38,36 @@
 				Follow
 			</view>
 		</view>
+		<view class="font-md my-1" >{{item.title}}</view>
+			<mymap :path="item.path" :center='item.center' :zoom='16' :mapheight='25' :post_id='item.post_id' ></mymap>
 		<!-- 标题 -->
-		<view class="font-md my-1" @click="openDetail">{{item.title}}</view>
+		
 		<!-- 帖子详情 -->
-		<slot>
-			<!-- 图片 -->
-			<image v-if="item.titlepic" :src="item.titlepic" @click="openDetail" 
-			style="height: 350rpx;" class="rounded w-100" mode="aspectFill"></image>
-		</slot>
+		
 		<!-- 图标按钮 -->
 		<view class="flex align-center">
+			
 			<!-- 顶 -->
 			<view class="flex align-center justify-center flex-1 animated faster"
-			hover-class="jello text-main" @click="doSupport('support')"
-			:class="item.support.type === 'support' ? 'support-active' : ''">
+			hover-class="jello text-main" 
+			>
 				<text class="iconfont icon-dianzan2 mr-2"></text>
-				<text>{{item.support.support_count > 0 ? item.support.support_count : 'Like'}}</text>
+				<text>{{item.like> 0 ? item.like : 'Like'}}</text>
 			</view>
 			<!-- 踩 -->
 			<view class="flex align-center justify-center flex-1 animated faster"
-			hover-class="jello text-main" @click="doSupport('unsupport')"
-			:class="item.support.type === 'unsupport' ? 'support-active' : ''">
+			hover-class="jello text-main"
+			>
 				<text class="iconfont icon-xihuan1 mr-2"></text> <!-- TODO:收藏图标 -->
-				<text>{{item.support.unsupport_count > 0 ? item.support.unsupport_count : 'Collect'}}</text>
+				<text>{{item.collect > 0 ? item.collect : 'Collect'}}</text>
 			</view>
 			<view class="flex align-center justify-center flex-1 animated faster"
-			hover-class="jello text-main" @click="doComment">
+			hover-class="jello text-main" >
 				<text class="iconfont icon-pinglun2 mr-2"></text>
-				<text>{{item.comment_count > 0 ? item.comment_count : 'Comment'}}</text>
+				<text>{{item.comments > 0 ? item.comments : 'Comment'}}</text>
 			</view>
 			<view class="flex align-center justify-center flex-1 animated faster"
-			hover-class="jello text-main" @click="doShare">
+			hover-class="jello text-main">
 				<text class="iconfont icon-fenxiang mr-2"></text>
 				<text>{{item.share_num > 0 ? item.share_num : 'Share'}}</text>
 			</view>
@@ -70,7 +78,9 @@
 <script>
 	import $T from '@/common/time.js';
 	import {mapState} from 'vuex';
+		import mymap from '../../components/map/mymap.vue';
 	export default {
+		components:{mymap},
 		props: {
 			item: Object,
 			index:{
@@ -119,20 +129,70 @@
 			},
 			// 进入详情页
 			openDetail(){
+				console.log('哈')
 				// 处于详情中
 				if (this.isdetail) return;
+				// uni.navigateTo({
+				// 	url: '../../pages/detail/detail?detail='+JSON.stringify(this.item),
+				// });
 				uni.navigateTo({
-					url: '../../pages/detail/detail?detail='+JSON.stringify(this.item),
+				  url: `../../pages/detail/detail?detail=${encodeURIComponent(JSON.stringify(this.item))}`,
 				});
-				// 加入历史记录
-				let list =  uni.getStorageSync('history')
-				list = list ? JSON.parse(list) : []
-				let index = list.findIndex(v=>v.id === this.item.id)
-				if(index === -1){
-					list.unshift(this.item)
-					uni.setStorageSync('history',JSON.stringify(list))
-				}
+				
 			},
+			// 进入详情页
+			//   async openDetail() {
+			//     console.log('进入详情页');
+			//     // 检查是否已经处于详情中
+			//     if (this.isdetail) return;
+			
+			//     // 准备发送到后端的用户ID
+			//     const userId = this.item.user_id; // 确保 this.item 有 user_id 属性
+			
+			//     try {
+			//       // 发送请求到后端判断用户是否VIP
+			//       const response = await uni.request({
+			//         url: `/vip/${userId}`, // 后端提供的相对路径
+			//         method: 'GET',
+			//         header: {
+			//           'content-type': 'application/json', // 根据服务器要求设置请求头
+			//         },
+			//       });
+			
+			//       // 检查后端返回的状态码
+			//       if (response.statusCode === 200) {
+			//         // 解析返回的JSON数据
+			//         const result = response.data;
+			
+			//         // 判断用户是否是VIP
+			//         if (result && result.isVip) {
+			//           // 如果是VIP，则跳转到详情页
+			//           uni.navigateTo({
+			//             url: `../../pages/detail/detail?detail=${encodeURIComponent(JSON.stringify(this.item))}`,
+			//           });
+			//         } else {
+			//           // 如果不是VIP，可以在这里处理非VIP用户的逻辑，比如提示用户
+			//           uni.showToast({
+			//             title: '您没有权限查看详情',
+			//             icon: 'none',
+			//           });
+			//         }
+			//       } else {
+			//         // 服务器返回非200状态码的处理逻辑
+			//         uni.showToast({
+			//           title: '服务器异常，请稍后再试',
+			//           icon: 'none',
+			//         });
+			//       }
+			//     } catch (error) {
+			//       // 网络请求出错的处理逻辑
+			//       console.error('请求失败:', error);
+			//       uni.showToast({
+			//         title: '网络请求失败，请检查网络',
+			//         icon: 'none',
+			//       });
+			//     }
+			//   },
 			// 顶踩操作
 			doSupport(type){
 				this.checkAuth(()=>{
---
+++ frontend(app)/components/map/mymap3.vue
@@ -0,0 +1,158 @@
+<template>
+	<view class="map" :style="{height:mapheight+'vh'}">
+	
+	<view id="amap" class="amap" style='width: 100%;
+			height: 100%;'></view>
+	</view>
+
+</template>
+
+
+<script module="ModuleInstance" lang="renderjs">
+	export default {
+		props: ['path', 'center', 'zoom', 'mapheight'],
+		data() {
+			return {
+				map: null,
+				layer: null,
+				parkList: [],
+				markerId: '',
+				longitude: 0,
+				latitude: 0,
+				markers: [ //标记点用于在地图上显示标记的位置
+					{
+						id: 1,
+						latitude: "", //纬度
+						longitude: "", //经度
+						iconPath: "/static/images/business_map/my.png", //图标路径
+						width: "30", //图标的宽
+						height: "30" //图标的高
+					}
+				]
+
+			};
+		},
+		watch: {
+		  path: function(newPath, oldPath) {
+			  console.log('path变了')
+		    // 当path变化时执行的操作
+		  },
+		  center: function(newCenter, oldCenter) {
+			    console.log('center变了')
+		    // 当center变化时执行的操作
+		  }
+		},
+		mounted() {
+			if (window.AMap) {
+				this.initAmap();
+			} else {
+				const script = document.createElement('script');
+				script.src = "https://webapi.amap.com/maps?v=1.4.15&key=399dacdae46d183be97ba277bda01549";
+				script.onload = () => {
+					this.initAmap();
+				}
+				document.head.appendChild(script);
+			}
+
+
+
+		},
+		created() {
+
+		},
+		
+		methods: {
+			myid(id){
+				return "mymap_"+id
+			},
+			initAmap() {
+				this.map = new AMap.Map(`amap`, {
+					zoom: this.zoom,
+					center: this.center,
+					mapStyle: 'amap://styles/57994c871bb604a4c79184f5f65d8782',
+					resizeEnable: true
+				});
+				this.map.on("complete", () => {
+					this.createLabelsLayer();
+				});
+				// 创建路径的坐标数组
+				// 创建 Polyline 对象并添加到地图上
+				var polyline = new AMap.Polyline({
+					path: this.path, // 设置路径数组
+					strokeColor: "#416dff", // 设置线颜色为红色
+					strokeOpacity: 0.8, // 设置线透明度为 0.8
+					strokeWeight: 5, // 设置线宽为 5
+					strokeStyle: "solid", // 设置线样式为虚线
+					strokeDasharray: [10, 5], // 设置虚线样式，间隔为 10 像素，实线长度为 5 像素
+					showDir: true
+				});
+				var marker = new AMap.Marker({
+					position: this.path[this.path.length - 1], // 终点坐标
+					map: this.map, // 所属的地图实例
+					icon: new AMap.Icon({ // 使用自定义图标
+						size: new AMap.Size(24, 30), // 图标尺寸
+						image: '//webapi.amap.com/theme/v1.3/markers/n/mark_r.png', // 图标地址
+						imageSize: new AMap.Size(24, 30) // 图标尺寸
+					})
+				});
+				polyline.setMap(this.map); // 将 Polyline 添加到地图上
+
+
+			},
+
+
+			createLabelsLayer() {
+				if (!this.map) return;
+				let that = this;
+				AMap.plugin('AMap.Geolocation', function() {
+					var geolocation = new AMap.Geolocation({
+						enableHighAccuracy: true,
+						timeout: 10000,
+						offset: [10, 20],
+						zoomToAccuracy: true,
+						position: 'RB'
+					})
+					geolocation.getCurrentPosition(function(status, result) {
+
+						if (status == 'complete') {
+							onComplete(result)
+						} else {
+							onError(result)
+						}
+					});
+
+					function onComplete(data) {
+						let marker = new AMap.Marker({
+							position: new AMap.LngLat(data.position.lng, data.position.lat),
+						});
+						that.map.setCenter(new AMap.LngLat(data.position.lng, data.position.lat));
+						that.map.setZoom(18);
+						that.map.add(marker);
+					}
+
+					function onError(data) {
+						console.log("定位出错", data);
+					}
+				})
+			}
+		},
+	};
+</script>
+
+<style lang="scss">
+	page {
+		width: 100%;
+		height: 80%;
+	}
+
+	.map {
+		width: 100%;
+
+
+	}
+
+	#amap {
+		width: 100%;
+		height: 100%;
+	}
+</style>
