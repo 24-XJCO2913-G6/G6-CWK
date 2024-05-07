2b68c271564e9da682b32de09413ece9ffed7473
周俊宇 <1771809893@qq.com>
2024-05-08 02:02:56 +0800

    [ADD]correct multiple para

--- backend/handlers/page_handler.go
+++ backend/handlers/page_handler.go
@@ -9,18 +9,23 @@ import (
 func ToIndex(c *gin.Context) {
 	Claim, _ := CheckToken(c.GetString("aToken"))
 	Uid := Claim.Uid
-
+	blogcount, _ := BlogCount(c)
+	followcount, _ := FollowCount(c)
+	followbycount, _ := FollowByCount(c)
+	signature, _ := SignatureCheck(c)
+	rank, _ := RankCheck()
+	blogs, _ := BlogDisplay()
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"message":     c.GetString("message"),
 		"aToken":      c.GetString("aToken"),
 		"rToken":      c.GetString("rToken"),
 		"Uid":         Uid,
-		"BlogCount":   BlogCount(c),      //发帖数
-		"FollowCount": FollowCount(c),    //关注数
-		"FansCount":   FollowByCount(c),  //粉丝数
-		"Signature":   SignatureCheck(c), //个签
-		"records":     RankCheck(),       //排名(降序) 距离：.Distance 用户名：.Name 头像：.Photo
-		"blogs":       BlogDisplay(),     //所有blog 用户名：.Author 头像：.Photo 发布时间：.Pub_time 可见性：.Visibility 内容：.Content 图片：.Picture 标签：.Tag
+		"BlogCount":   blogcount,     //发帖数
+		"FollowCount": followcount,   //关注数
+		"FansCount":   followbycount, //粉丝数
+		"Signature":   signature,     //个签
+		"records":     rank,          //排名(降序) 距离：.Distance 用户名：.Name 头像：.Photo
+		"blogs":       blogs,         //所有blog 用户名：.Author 头像：.Photo 发布时间：.Pub_time 可见性：.Visibility 内容：.Content 图片：.Picture 标签：.Tag
 	})
 }
 
@@ -42,17 +47,23 @@ func ToRegister(c *gin.Context) {
 
 func ToProfile(c *gin.Context, uid string) {
 	Uid, _ := strconv.ParseInt(uid, 10, 64)
+	name, _ := GetName(c)
+	email, _ := GetEmail(c)
+	photo, _ := GetPhoto(c)
+	motto, _ := GetSignature(c)
+	likelist, _ := GetLike(c)
+	journey, _ := GetTracks(Uid)
 	c.HTML(http.StatusOK, "my-profile.html", gin.H{
-		"uid":       uid,
-		"message":   c.GetString("message"),
-		"aToken":    c.GetString("aToken"),
-		"rToken":    c.GetString("rToken"),
-		"name":      GetName(c),
-		"email":     GetEmail(c),
-		"photo":     GetPhoto(c),
-		"signature": GetSignature(c),
-		"likeList":  GetLike(c),
-		"journey":   GetTracks(Uid),
+		"uid":      uid,
+		"message":  c.GetString("message"),
+		"aToken":   c.GetString("aToken"),
+		"rToken":   c.GetString("rToken"),
+		"username": name,
+		"email":    email,
+		"photo":    photo,
+		"motto":    motto,
+		"likeList": likelist,
+		"journey":  journey,
 	})
 }
 
@@ -137,23 +148,25 @@ func ToPostDetails(Id string, c *gin.Context) {
 	Bid, _ := strconv.ParseInt(Id, 10, 64)
 	blog, _ := GetBlog(Bid)
 	track, _ := GetTrack(blog.Tid)
+	reviews, _ := GetReviews(Bid)
+	photo, _ := GetPhoto(c)
 	c.HTML(http.StatusOK, "post-details.html", gin.H{
 		"message": c.GetString("message"),
 		"aToken":  c.GetString("aToken"),
 		"rToken":  c.GetString("rToken"),
-		"photo":   GetPhoto(c),
+		"photo":   photo,
 		"blog":    blog,
 		"track":   track,
-		"reviews": GetReviews(Bid), //评论者.Reviewer 评论者头像.Reviewer_photo 时间.Time 内容.Content
+		"reviews": reviews, //评论者.Reviewer 评论者头像.Reviewer_photo 时间.Time 内容.Content
 	})
 }
-
 func ToLog(c *gin.Context) {
+	photo, _ := GetPhoto(c)
 	c.JSON(http.StatusOK, gin.H{
 		"message": c.GetString("message"),
 		"aToken":  c.GetString("aToken"),
 		"rToken":  c.GetString("rToken"),
-		"photo":   GetPhoto(c),
+		"photo":   photo,
 		"browser": GetBrowser(c),
 	})
 }
