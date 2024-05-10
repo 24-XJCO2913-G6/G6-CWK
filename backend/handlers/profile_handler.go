c03aca7f149f914a457dd0c2cb4a376318c80c99
NS <sc21sn@leeds.ac.uk>
2024-05-10 21:15:51 +0800

    [FIX] fix the pointer bugs

--- backend/handlers/profile_handler.go
+++ backend/handlers/profile_handler.go
@@ -1,175 +1,156 @@
 package handlers
 
 import (
-	"github.com/gin-gonic/gin"
 	. "main/backend/models"
-	"strconv"
 )
 
 func GetName(Uid int64) (string, error) {
 
-	var users []User
-	if Uid != -1 {
-		err1 := Db.Where("Uid = ?", Uid).Find(&users)
-		if err1 != nil {
-			return "null", err1
-		}
+	var user = &User{Uid: Uid}
+	if Uid == -1 {
+		return "null", nil
 	}
-	Name := users[0].Name
-	return Name, nil
+	has, err := Db.Get(user)
+	if !has || err != nil {
+		return "null", nil
+	}
+	return user.Name, nil
 }
 
 func GetEmail(Uid int64) (string, error) {
 
-	var users []User
-	if Uid != -1 {
-		err1 := Db.Where("Uid = ?", Uid).Find(&users)
-		if err1 != nil {
-			return "null", err1
-		}
+	var user = &User{Uid: Uid}
+	if Uid == -1 {
+		return "null", nil
 	}
-	Email := users[0].Email
-	return Email, nil
+	has, err := Db.Get(user)
+	if !has || err != nil {
+		return "null", nil
+	}
+	return user.Email, nil
 }
 
 func GetPhoto(Uid int64) (string, error) {
 
-	var users []User
-	if Uid != -1 {
-		err1 := Db.Where("Uid = ?", Uid).Find(&users)
-		if err1 != nil {
-			return "null", err1
-		}
+	var user = &User{Uid: Uid}
+	if Uid == -1 {
+		return "null", nil
 	}
-	ProfilePhot := users[0].ProfilePhot
-	return ProfilePhot, nil
+	has, err := Db.Get(user)
+	if !has || err != nil {
+		return "null", nil
+	}
+	return user.ProfilePhot, nil
 }
 func GetSignature(Uid int64) (string, error) {
 
-	var users []User
-	if Uid != -1 {
-		err1 := Db.Where("Uid = ?", Uid).Find(&users)
-		if err1 != nil {
-			return "null", err1
-		}
+	var user = &User{Uid: Uid}
+	if Uid == -1 {
+		return "null", nil
 	}
-	Signature := users[0].Signature
-	return Signature, nil
+	has, err := Db.Get(user)
+	if !has || err != nil {
+		return "null", nil
+	}
+	return user.Name, nil
 }
 
 func GetLike(Uid int64) ([]Liked, error) {
-	var likedBlogs []Liked
-	var likeList []Like
-	var blogLiked []Blog
-	var reviews []Review
-	var likeCount []Like
+	var likedBlogs = new([]Liked)
+	var likeList = new([]Like)
+	var blogLiked = new([]Blog)
+	var reviews = new([]Review)
+	var likeCount = new([]Like)
 	var reviewAmount int64 = 0
 	var likeAmount int64 = 0
 	if Uid != -1 {
-		err1 := Db.Where("Uid = ?", Uid).Find(&likeList)
-		if err1 != nil {
-			return []Liked{}, err1
-		}
+		return nil, nil
 	}
-	for _, liked := range likeList {
+	has, err := Db.Get(likeList)
+	if !has || err != nil {
+		return nil, nil
+	}
+	for _, liked := range *likeList {
 		Id := liked.Bid
-		err2 := Db.Where("Id = ?", Id).Find(&blogLiked)
+		err2 := Db.Where("Id = ?", Id).Find(blogLiked)
 		if err2 != nil {
-			return []Liked{}, err2
+			return nil, err2
 		}
-		err3 := Db.Where("Bid = ?", Id).Find(&reviews)
+		err3 := Db.Where("Bid = ?", Id).Find(reviews)
 		if err3 != nil {
-			return []Liked{}, err3
+			return nil, err3
 		}
-		for range reviews {
-			reviewAmount += 1
-		}
-		err4 := Db.Where("Bid = ?", Id).Find(&likeCount)
+		err4 := Db.Where("Bid = ?", Id).Find(likeCount)
 		if err4 != nil {
-			return []Liked{}, err4
+			return nil, err4
 		}
-		for range likeCount {
-			likeAmount += 1
-		}
-		likedBlog := Liked{Bid: blogLiked[0].Id, Content: blogLiked[0].Content, Picture: blogLiked[0].Picture, Title: blogLiked[0].Title, Visibility: blogLiked[0].Visibility, Time: blogLiked[0].Pub_time, Review: reviews, ReviewCount: reviewAmount, LikeCount: likeAmount}
-		likedBlogs = append(likedBlogs, likedBlog)
+		reviewAmount = int64(len(*reviews))
+		likeAmount = int64(len(*likeCount))
+		likedBlog := Liked{Bid: (*blogLiked)[0].Id, Content: (*blogLiked)[0].Content, Picture: (*blogLiked)[0].Picture, Title: (*blogLiked)[0].Title, Visibility: (*blogLiked)[0].Visibility, Time: (*blogLiked)[0].Pub_time, Review: *reviews, ReviewCount: reviewAmount, LikeCount: likeAmount}
+		*likedBlogs = append(*likedBlogs, likedBlog)
 	}
-	return likedBlogs, nil
+	return *likedBlogs, nil
+
 }
-func GetCollect(c *gin.Context) ([]Collected, error) {
-	var collectedBlogs []Collected
-	var collectList []Collect
-	var blogCollect []Blog
-	var reviews []Review
-	var collectCount []Collect
+func GetCollect(Uid int64) ([]Collected, error) {
+	var collectedBlogs = new([]Collected)
+	var collectList = new([]Collect)
+	var blogCollect = new([]Blog)
+	var reviews = new([]Review)
+	var collectCount = new([]Collect)
 	var reviewAmount int64 = 0
 	var collectAmount int64 = 0
-	aToken := c.GetHeader("aToken")
-	var Uid_tmp string
-	if aToken == "" {
-		Uid_tmp = "-1"
-	} else {
-		Claim, _ := CheckToken(aToken)
-		Uid_tmp = Claim.Uid
+	if Uid == -1 {
+		return nil, nil
 	}
-	Uid, err := strconv.ParseInt(Uid_tmp, 10, 64)
-	if (Uid == -1) || (err != nil) {
-		err1 := Db.Where("Uid = ?", Uid).Find(&collectList)
-		if err1 != nil {
-			return []Collected{}, err1
-		}
-	}
-	for _, liked := range collectList {
+	for _, liked := range *collectList {
 		Id := liked.Bid
-		err2 := Db.Where("Id = ?", Id).Find(&blogCollect)
+		err2 := Db.Where("Id = ?", Id).Find(blogCollect)
 		if err2 != nil {
-			return []Collected{}, err2
+			return nil, err2
 		}
-		err3 := Db.Where("Bid = ?", Id).Find(&reviews)
+		err3 := Db.Where("Bid = ?", Id).Find(reviews)
 		if err3 != nil {
-			return []Collected{}, err3
+			return nil, err3
 		}
-		for range reviews {
-			reviewAmount += 1
-		}
-		err4 := Db.Where("Bid = ?", Id).Find(&collectCount)
+		reviewAmount = int64(len(*reviews))
+		err4 := Db.Where("Bid = ?", Id).Find(collectCount)
 		if err4 != nil {
 			return []Collected{}, err4
 		}
-		for range collectCount {
-			collectAmount += 1
-		}
-		collectedBlog := Collected{Bid: blogCollect[0].Id, Content: blogCollect[0].Content, Picture: blogCollect[0].Picture, Title: blogCollect[0].Title, Visibility: blogCollect[0].Visibility, Time: blogCollect[0].Pub_time, Review: reviews, ReviewCount: reviewAmount, CollectCount: collectAmount}
-		collectedBlogs = append(collectedBlogs, collectedBlog)
+		collectAmount = int64(len(*collectCount))
+		collectedBlog := Collected{Bid: (*blogCollect)[0].Id, Content: (*blogCollect)[0].Content, Picture: (*blogCollect)[0].Picture, Title: (*blogCollect)[0].Title, Visibility: (*blogCollect)[0].Visibility, Time: (*blogCollect)[0].Pub_time, Review: (*reviews), ReviewCount: reviewAmount, CollectCount: collectAmount}
+		*collectedBlogs = append(*collectedBlogs, collectedBlog)
 	}
-	return collectedBlogs, nil
+	return *collectedBlogs, nil
 }
-func GetJourney(c *gin.Context) (string, error) {
 
-	var users []User
-	aToken := c.GetHeader("aToken")
-	var Uid_tmp string
-	if aToken == "" {
-		Uid_tmp = "-1"
-	} else {
-		Claim, _ := CheckToken(aToken)
-		Uid_tmp = Claim.Uid
-	}
-	Uid, err := strconv.ParseInt(Uid_tmp, 10, 64)
-	if (Uid == -1) || (err != nil) {
-		err1 := Db.Where("Uid = ?", Uid).Find(&users)
-		if err1 != nil {
-			return "null", err1
-		}
-	}
-	Signature := users[0].Signature
-	return Signature, nil
-}
-func GetInfo(Uid int64) (User_detail, error) {
-	var info User_detail
-	err := Db.Where("uid = ?", Uid).Find(&info)
-	if err != nil {
-		return User_detail{}, err
-	}
-	return info, nil
-}
+//func GetJourney(c *gin.Context) (string, error) {
+//
+//	var users []User
+//	aToken := c.GetHeader("aToken")
+//	var Uid_tmp string
+//	if aToken == "" {
+//		Uid_tmp = "-1"
+//	} else {
+//		Claim, _ := CheckToken(aToken)
+//		Uid_tmp = Claim.Uid
+//	}
+//	Uid, err := strconv.ParseInt(Uid_tmp, 10, 64)
+//	if (Uid == -1) || (err != nil) {
+//		err1 := Db.Where("Uid = ?", Uid).Find(&users)
+//		if err1 != nil {
+//			return "null", err1
+//		}
+//	}
+//	Signature := users[0].Signature
+//	return Signature, nil
+//}
+//func GetInfo(Uid int64) (User_detail, error) {
+//	var info User_detail
+//	err := Db.Where("uid = ?", Uid).Find(&info)
+//	if err != nil {
+//		return User_detail{}, err
+//	}
+//	return info, nil
+//}
